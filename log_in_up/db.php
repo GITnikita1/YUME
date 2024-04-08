d45a6fd9f253ea70fda11322a8057de424e7f32b
Nikolay Belohvosticov <KoLeyBeLiey14102004@mail.ru>
2024-04-08 23:03:37 +0300

    0

--- log_in_up/db.php
+++ log_in_up/db.php
@@ -1,11 +1,11 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "YUME";
+$servername = "k90653re.beget.tech";
+$username = "k90653re_yume";
+$password = "Mysql_YUME_12345";
+$dbname = "k90653re_yume";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername,$username,$password, $dbname);
 
 if (!$conn) {
     die("Connection Fialed". mysqli_connect_error());
