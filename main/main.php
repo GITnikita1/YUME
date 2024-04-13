eeb85274ea7659aa0f284ddb18dc7df444e69fdb
KarKirill <karasevkirill2004@mail.ru>
2024-04-13 21:05:42 +0300

    Реализация <footer>

---
+++ main/main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/main/main.css">
+    <link rel="shortcut icon" href="/img/BurgerIcon.ico"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Primary list</title>
+</head>
+<body>
+    <footer>
+        <div class="_upper">
+            <div class="_about">
+                <table>
+                    <tr>
+                        <th style="text-align: left;">О нас</th>
+                        <th>Заказ</th>
+                        <th>Адреса</th>
+                        <th>Меню</th>
+                        <th>Связаться с нами</th>
+                    </tr>
+                    <tr>
+                        <th colspan="3" style="text-align: left;">Политика конфиденциальности</th>
+                    </tr>
+                </table>
+            </div>
+            <div class="_socnet">
+            <table>
+                    <tr>
+                        <th><i class="fa-brands fa-vk"></i></th>
+                        <th><i class="fa-brands fa-instagram"></i></th>
+                        <th><i class="fa-brands fa-facebook"></i></th>
+                        <th><i class="fa-brands fa-twitter"></i></th>
+                        <th><i class="fa-brands fa-tiktok"></i></th>
+                        <th><i class="fa-solid fa-face-grin-tongue-squint"></i></th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <hr width="100%">
+        <div class="_down">
+            <p>@2024, CodeEaters Здесь! Все права у нас.</p>
+        </div>
+    </footer>
+</body>
+</html>
