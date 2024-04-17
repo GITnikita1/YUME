52d85095ff6ebccd473a609af62a66230c4fc8e6
Nikita Belohvostikov <nikitabeliy2004@mail.ru>
2024-04-17 22:40:35 +0300

    1

--- blocks/header.php
+++ blocks/header.php
@@ -1,16 +1,16 @@
 <link rel="stylesheet" href="../blocks/headercss.css">
 <nav>
-    <div class="_logo"><i class="fa-solid fa-face-grin-tongue-squint"></i></div>
+    <div class="_logo"><a href="../main/main.php"><i class="fa-solid fa-face-grin-tongue-squint"></i></a></div>
     <div class="strip">
         <table class="strip_menu">
             <tr>
-                <th class="main">Главная</th>
-                <th class="menu">Меню</th>
-                <th class="map">Карта</th>
-                <th class="about_us">О нас</th>
+                <!-- <th class="main"><a href="../main/main.php">Главная</a></th> -->
+                <th class="menu"><a href="../menu/menu.php">Меню</a></th>
+                <th class="map"><a href="../map/map.php">Карта</a></th>
+                <th class="about_us"><a href="../about_us/about_us.php">О нас</a></th>
                 <th><i class="fa-regular fa-moon"></i></th>
                 <th><i class="fa-solid fa-language"></i></th>
-                <th class="basket"><i class="fa-solid fa-basket-shopping"></i></th>
+                <th class="basket"><a href="../basket/basket.php"><i class="fa-solid fa-basket-shopping"></i></a></th>
             </tr>
         </table>
     </div>
