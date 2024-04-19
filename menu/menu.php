6a22e19b1eae0d717c156f07a886b596e7b7acc6
KarKirill <karasevkirill2004@mail.ru>
2024-04-19 22:01:55 +0300

    1111

--- menu/menu.php
+++ menu/menu.php
@@ -5,12 +5,111 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/img/BurgerIcon.ico"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/menu/menu.css">
     <title>Меню</title>
 </head>
 <body>
     <?php include "..\blocks\header.php"; ?>
 
+    <div class="spine">
 
+        <div class="greating">
+            <h2>МЕНЮ НЕДЕЛИ</h2>
+            <p style="font-size: 3.5rem;"><span class="left">15 АПРЕЛЯ</span><span> : </span><span class="right">22 АПРЕЛЯ</span></p>
+            <p>Наслаждайтесь вкусом Недели! Бургерная ЯМИ. представляет новые, ошеломляющие вкусовые бургеры каждую неделю. Не можете оторваться от любимого? Не волнуйтесь, мы возвращаем их для вашего повторного удовольствия. Приходите сегодня и попробуйте разницу!</p>
+        </div>
+
+        <div class="container .seven">
+            <h3>Бургеры</h3>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h3>Бургеры</h3>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h3>Бургеры</h3>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h3>Бургеры</h3>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+            <div class="box">
+                <div class="picture"></div>
+                <div class="cost"></div>
+            </div>
+        </div>
 
     <?php include '..\blocks\footer.php'; ?>
 </body>
