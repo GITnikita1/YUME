82dca985b2193ee7c9a18e975516ae5dc7199235
KarKirill <karasevkirill2004@mail.ru>
2024-04-20 01:05:32 +0300

    9999

--- about_us/about_us.php
+++ about_us/about_us.php
@@ -5,12 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/img/BurgerIcon.ico"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/about_us/about_us.css">
     <title>О нас</title>
 </head>
 <body>
     <?php include "..\blocks\header.php"; ?>
 
+    <header>
+        <img src="/img/main_img.jpeg" alt="" width="100%">
+    </header>
 
+    <div class="spine">
+        <div class="container1">
+            <h3>О НАС</h3>
+            <div class="text"><p>ЯМИ - молодой бренд, специализирующийся на продаже бургеров, которые готовятся с использованием только натуральных ингредиентов высокого качества. </p></div>
+            <div class="us"></div>
+        </div>
+
+        <div class="container">
+            <div class="left"></div>
+            <div class="right" style="background-color: #FD8A5E;    padding: 1rem;">
+                <h3 style="color:#1D1D1D">НАШ КОЛЛЕКТИВ</h3>
+                <div class="text2"><p>Наш коллектив профессионалов, имеющих богатый опыт работы в сфере ресторанного бизнеса, стремится создать уникальный и неповторимый вкус каждого из наших бургеров. Мы используем только свежие ингредиенты, такие как мясо говядины, курица, свинина, овощи и сыры, чтобы создать настоящие шедевры гастрономии.</p></div>
+            </div>
+        </div>
+
+        <div class="container3">
+        <h3>НАША МИССИЯ</h3>
+        <div class="text"><p>Предложить нашим клиентам лучшие вкусы и ароматы бургеров, используя только натуральные ингредиенты высокого качества. Мы стремимся создать уникальный и неповторимый вкус каждого из наших бургеров, чтобы наш ресторан стал любимым местом для всех любителей бургеров.</p></div>
+        </div>
+
+        <div class="container">
+            <div class="left" style="background-color: #C7F465;    padding: 1rem;">
+                <h3 style="color:#1D1D1D">ЧТО ПОКУШАТЬ?</h3>
+                <div class="text2"><p>В ЯМИ вы найдете широкий ассортимент бургеров, которые подходят для любого вкуса и предпочтений. Мы предлагаем классические бургеры, такие как бургер с говядиной, куриный бургер, а также наши собственные эксклюзивные рецепты, такие как бургер с лобстером, бургер с омаром и многие другие.</p></div>
+            </div>
+            <div class="right"></div>
+        </div>
+
+        <div class="container5">
+        <h3>СКОРО УВИДЕМСЯ!</h3>
+        <div class="text"><p>Предложить нашим клиентам лучшие вкусы и ароматы бургеров, используя только натуральные ингредиенты высокого качества. Мы стремимся создать уникальный и неповторимый вкус каждого из наших бургеров, чтобы наш ресторан стал любимым местом для всех любителей бургеров.</p></div>
+        </div>
+    </div>
+
+    <div class="last">
+        <div class="left2"></div>    
+        <div class="right2"></div>
+    </div>
 
     <?php include '..\blocks\footer.php'; ?>
 </body>
